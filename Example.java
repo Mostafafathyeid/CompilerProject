bbc3603e9f696cb11b79da8aa968717571623140
Mostafafathyeid <mostafa180845@feng.bu.edu.eg>
2023-03-20 22:19:19 +0200

    Initial commit

---
+++ Example.java
@@ -0,0 +1,14 @@
+public class Example {
+    public static void main(String[] args) {
+            int x=5;
+            if(x==5){
+                System.out.println(x);
+        }
+        if(true){
+
+        }
+        else{
+
+        }
+    }
+}
