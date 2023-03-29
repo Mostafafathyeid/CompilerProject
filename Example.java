3fce87c9b4f027539af69d0d2711b886fdd1ba8a
Mostafafathyeid <66316400+Mostafafathyeid@users.noreply.github.com>
2023-03-29 21:59:39 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,6 +1,5 @@
-import java.io.*;
 public class Example {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
             int x=5;
             if(x==5){
                 System.out.println(x);
