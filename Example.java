e057e02e1366f39a55db574c34e57698d9e0c1cc
yousef-ahmed-231 <66273172+yousef-ahmed-231@users.noreply.github.com>
2023-03-29 16:53:57 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,14 +1,16 @@
+import java.io.*;
 public class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
             int x=5;
             if(x==5){
                 System.out.println(x);
-        }
-        if(true){
+            }
+            else{
+                System.out.println("error");
+            }
+            for(int i=0;i<x;i++){
+            }
 
-        }
-        else{
 
-        }
     }
 }
